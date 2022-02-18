89c1ef8ef8670b46edb62be56f4da774c4c04358
bustobri000 <bustobri000@warren.k12.in.us>
2022-02-18 13:11:02 -0500

    power of NEO

--- src/org/WalkerCC_CompSci/Rectangle.java
+++ src/org/WalkerCC_CompSci/Rectangle.java
@@ -5,7 +5,7 @@ public class Rectangle extends Quadrilateral{
     public Rectangle(Point startingPoint, double length, double width){
         super(startingPoint,
                 new Point(startingPoint.getX() + length, startingPoint.getY()),
-                new Point(startingPoint.getX(), startingPoint.getY() + width),
-                new Point(startingPoint.getX() + length, startingPoint.getY() + width));
+                new Point(startingPoint.getX() + length, startingPoint.getY() + width),
+                new Point(startingPoint.getX(), startingPoint.getY() + width));
     }
 }
--- src/org/WalkerCC_CompSci/Shape.java
+++ src/org/WalkerCC_CompSci/Shape.java
@@ -18,12 +18,26 @@ public abstract class Shape {
 
     private double CalculateSideLength(Point point0, Point point1){
         //Does the GetSideLength math work stuff
+        //distance
+        //sqrt( (x1 - x2)^2 + (y1 - y2)^2 )
+        double x0 = point0.getX();
+        double y0 = point0.getY();
+        double x1 = point1.getX();
+        double y1 = point1.getY();
+        double x = x1 - x0;
+        double y = y1 - y0;
+        double distance;
+        distance = Math.pow(x, 2) + Math.pow(y, 2);
+        return Math.sqrt(distance);
     }
 
     public double GetPerimeter(){
         //Does the Perimeter Math Work Stuff
+        double perimeter = 0;
+        for (int i =  0; i < listOfPoints.length; i = i + 1){
+            perimeter += GetSideLength(i);
+        }
+        return perimeter;
     }
-
-
 
 }
